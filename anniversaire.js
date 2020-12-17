b2f0512d43ed2e056b095fc02c6122d735ce8b8d
Mdubois <maxime.dubois60185@gmail.com>
2020-12-17 14:40:46 +0100

    test2

--- anniversaire.js
+++ anniversaire.js
@@ -2,7 +2,7 @@ $(function() {
 
     $("p").hide();
     $("#gif").hide();
-    $("body").css("background"; "#272822")
+    $("body").css("background"; "white")
 
     $("h1").on( "click", function() {
         $("p:first").show();
