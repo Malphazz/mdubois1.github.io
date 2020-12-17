c0d8692000aaf16d55d128cac005cd6ef132b208
Mdubois <maxime.dubois60185@gmail.com>
2020-12-17 14:37:21 +0100

    test2

--- anniversaire.js
+++ anniversaire.js
@@ -2,7 +2,7 @@ $(function() {
 
     $("p").hide();
     $("#gif").hide();
-    $("body").css("background" , "white")
+    $("body").css("background" , "black")
 
     $("h1").on( "click", function() {
         $("p:first").show();
